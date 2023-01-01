6a21ff92452355d89c107823525bb9693bb12ab9
Dafferianto Trinugroho <daft@equinor.com>
2023-01-01 12:32:08 +0100

    AoC 2022 day 16 part 2

--- 2022/day16/nodejs/day16.test.js
+++ 2022/day16/nodejs/day16.test.js
@@ -1,6 +1,6 @@
 import fs from "fs";
 import assert from "assert";
-import { day16Part1, day16Part2 } from "./day16.js";
+import { day16Part1, day16Part2, getPermuteTwo } from "./day16.js";
 
 describe("day 16 part 1", () => {
   it("case 1", () => {
@@ -11,10 +11,29 @@ describe("day 16 part 1", () => {
   });
 });
 
+describe("test getPermuteTwo", () => {
+  it("case 1", () => {
+    let valvesMap = { A: true, B: true };
+    const result = getPermuteTwo(valvesMap);
+    assert.equal(result.length, 2);
+  });
+  it("case 2", () => {
+    let valvesMap = { A: true, B: true, C: true };
+    const result = getPermuteTwo(valvesMap);
+    assert.equal(result.length, 6);
+  });
+  it("case 3", () => {
+    let valvesMap = { A: true, B: true, C: true, D: true };
+    const result = getPermuteTwo(valvesMap);
+    assert.equal(result.length, 12);
+  });
+});
+
 describe("day 16 part 2", () => {
   it("case 1", () => {
-    const inputArr = [];
+    const input = fs.readFileSync("input_test.txt", "utf8");
+    const inputArr = input.split("\n");
     const result = day16Part2(inputArr);
-    assert.equal(result, 0);
+    assert.equal(result, 1707);
   });
 });
