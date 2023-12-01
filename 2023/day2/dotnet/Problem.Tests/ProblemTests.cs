e996e90ec137aeee824190fd2cd9d10ec883bca0
Dafferianto Trinugroho <daft@equinor.com>
2023-12-01 21:32:59 +0100

    AoC 2023 day 2 part 2 AoC 2023 day 2

---
+++ 2023/day2/dotnet/Problem.Tests/ProblemTests.cs
@@ -0,0 +1,34 @@
+namespace Problem.Tests;
+
+public class ProblemTests
+{
+    [Fact]
+    public void Part1Test1()
+    {
+        var inputList = new List<string>
+        {
+            "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+            "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+            "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+            "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+            "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+        };
+        var result = Problem.ProblemPart1(inputList);
+        Assert.Equal(8, result);
+    }
+
+    [Fact]
+    public void Part2Test1()
+    {
+        var inputList = new List<string>
+        {
+            "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+            "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+            "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+            "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+            "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+        };
+        var result = Problem.ProblemPart2(inputList);
+        Assert.Equal(2286, result);
+    }
+}
