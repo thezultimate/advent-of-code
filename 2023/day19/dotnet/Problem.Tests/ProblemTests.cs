a49aa41d20b8788da402e05e90895532a01ac654
Dafferianto Trinugroho <daft@equinor.com>
2023-12-18 22:42:13 +0100

    AoC 2023 day 19 skeleton

---
+++ 2023/day19/dotnet/Problem.Tests/ProblemTests.cs
@@ -0,0 +1,16 @@
+namespace Problem.Tests;
+
+public class ProblemTests
+{
+    [Fact]
+    public void Part1Test1()
+    {
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void Part2Test1()
+    {
+        Assert.Equal(0, result);
+    }
+}
---
+++ 2023/day19/dotnet/Problem/Problem.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics.Metrics;
+using System.Text.RegularExpressions;
+
+namespace Problem;
+public class Problem
+{
+    public static long ProblemPart1(List<string> inputList)
+    {
+        return 0;
+    }
+
+    public static long ProblemPart2(List<string> inputList)
+    {
+        return 0;
+    }
+}
