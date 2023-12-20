f531712fe1ba93859f3aa836b33a2e38fceea38b
Dafferianto Trinugroho <daft@equinor.com>
2023-12-21 00:18:45 +0100

    AoC 2023 day 21 part 1

---
+++ 2023/day21/dotnet/Main/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Main;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var inputArr = File.ReadAllLines("input.txt");
+        var inputList = new List<string>(inputArr);
+        var resultPart1 = Problem.Problem.ProblemPart1(inputList, 64);
+        // var resultPart2 = Problem.Problem.ProblemPart2(inputList);
+        Console.WriteLine(resultPart1);
+        // Console.WriteLine(resultPart2);
+    }
+}
