f47d31aab053059fdc27d5c7c7192b08963d346a
Dafferianto Trinugroho <daft@equinor.com>
2023-12-30 00:03:19 +0100

    AoC 2023 day 17 part 2

--- 2023/day17/dotnet/Problem.Tests/ProblemTests.cs
+++ 2023/day17/dotnet/Problem.Tests/ProblemTests.cs
@@ -30,8 +30,36 @@ public class ProblemTests
     {
         var inputList = new List<string>
         {
+            "2413432311323",
+            "3215453535623",
+            "3255245654254",
+            "3446585845452",
+            "4546657867536",
+            "1438598798454",
+            "4457876987766",
+            "3637877979653",
+            "4654967986887",
+            "4564679986453",
+            "1224686865563",
+            "2546548887735",
+            "4322674655533"
         };
         var result = Problem.ProblemPart2(inputList);
-        Assert.Equal(0, result);
+        Assert.Equal(94, result);
+    }
+
+    [Fact]
+    public void Part2Test2()
+    {
+        var inputList = new List<string>
+        {
+            "111111111111",
+            "999999999991",
+            "999999999991",
+            "999999999991",
+            "999999999991"
+        };
+        var result = Problem.ProblemPart2(inputList);
+        Assert.Equal(71, result);
     }
 }
