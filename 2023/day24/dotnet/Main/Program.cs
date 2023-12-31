f7260a569ebbe0640f9c113a387ec9573b97b7c3
Dafferianto Trinugroho <daft@equinor.com>
2023-12-31 10:04:01 +0100

    AoC 2023 day 24 part 2

--- 2023/day24/dotnet/Main/Program.cs
+++ 2023/day24/dotnet/Main/Program.cs
@@ -6,8 +6,8 @@ class Program
         var inputArr = File.ReadAllLines("input.txt");
         var inputList = new List<string>(inputArr);
         var resultPart1 = Problem.Problem.ProblemPart1(inputList, 200000000000000, 400000000000000);
-        // var resultPart2 = Problem.Problem.ProblemPart2(inputList);
         Console.WriteLine(resultPart1);
-        // Console.WriteLine(resultPart2);
+        var resultPart2 = Problem.Problem.ProblemPart2(inputList);
+        Console.WriteLine(resultPart2);
     }
 }
