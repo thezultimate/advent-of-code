6617ffb40b4f4a0cc6fb98c34c4333544db5764a
Dafferianto Trinugroho <daft@equinor.com>
2024-12-17 21:59:01 +0100

    AoC 2024 day 18

---
+++ 2024/day18/python/test_problem.py
@@ -0,0 +1,66 @@
+import unittest
+from problem import problem_part1, problem_part2
+
+class TestProblem(unittest.TestCase):
+    def test_problem_part1_test1(self):
+        input_list = [
+            "5,4",
+            "4,2",
+            "4,5",
+            "3,0",
+            "2,1",
+            "6,3",
+            "2,4",
+            "1,5",
+            "0,6",
+            "3,3",
+            "2,6",
+            "5,1",
+            "1,2",
+            "5,5",
+            "2,5",
+            "6,5",
+            "1,4",
+            "0,4",
+            "6,4",
+            "1,1",
+            "6,1",
+            "1,0",
+            "0,5",
+            "1,6",
+            "2,0"
+        ]
+        result = problem_part1(input_list)
+        self.assertEqual(result, 22)
+
+
+    def test_problem_part2_test1(self):
+        input_list = [
+            "5,4",
+            "4,2",
+            "4,5",
+            "3,0",
+            "2,1",
+            "6,3",
+            "2,4",
+            "1,5",
+            "0,6",
+            "3,3",
+            "2,6",
+            "5,1",
+            "1,2",
+            "5,5",
+            "2,5",
+            "6,5",
+            "1,4",
+            "0,4",
+            "6,4",
+            "1,1",
+            "6,1",
+            "1,0",
+            "0,5",
+            "1,6",
+            "2,0"
+        ]
+        result = problem_part2(input_list)
+        self.assertEqual(result, "6,1")
