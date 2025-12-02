c39e1a8331e50b8e6b6f197a7a72a014a4b7c927
Dafferianto Trinugroho <daft@equinor.com>
2025-12-02 06:28:42 +0100

    AoC 2025 day 2 Julia

---
+++ 2025/day2/julia/TestProblem.jl
@@ -0,0 +1,11 @@
+using Test
+
+include("Problem.jl")
+
+@testset "Problem" begin
+    input_string = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
+    
+    @test problem_part1(input_string) == 1227775554
+
+    @test problem_part2(input_string) == 4174379265
+end
