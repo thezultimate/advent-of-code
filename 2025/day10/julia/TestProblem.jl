1f85cc7eeccba137c7751dac0c493a42b060ca66
Dafferianto Trinugroho <daft@equinor.com>
2025-12-10 12:14:28 +0100

    AoC 2025 day 10 part 1 Julia

---
+++ 2025/day10/julia/TestProblem.jl
@@ -0,0 +1,14 @@
+using Test
+
+include("Problem.jl")
+
+@testset "Problem" begin
+    input_vector = [
+        "[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}",
+        "[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}",
+        "[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}"
+    ]
+    @test problem_part1(input_vector) == 7
+
+    @test problem_part2(input_vector) == 33
+end
